f1693b2181650336346ad95a38aa8731bec7ad93
EC2 Default User <ec2-user@ip-172-31-3-162.ap-northeast-1.compute.internal>
2023-04-27 07:52:24 +0000

    Initial Commit

--- db/migrate/20230410104346_create_books.rb
+++ db/migrate/20230410104346_create_books.rb
@@ -4,7 +4,7 @@ class CreateBooks < ActiveRecord::Migration[6.1]
       
       t.string :title
       t.string :body
-      t.integer :id
+      t.integer :user_id
       t.timestamps
     end
   end
