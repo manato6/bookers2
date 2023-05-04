70e69d043a6f903cc49fe158d518015c49be2e8f
EC2 Default User <ec2-user@ip-172-31-3-162.ap-northeast-1.compute.internal>
2023-05-04 07:09:48 +0000

    Rspecテスト修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,9 +9,11 @@ class BooksController < ApplicationController
     @book.user_id = current_user.id
     if @book.save
     flash[:notice] = "you have created book successfully."
-    redirect_to books_path
+    redirect_to book_path(@book.id)
     else
-      redirect_to :index
+       @user = current_user
+      @books = Book.all
+      render :index
     end
     end
 
